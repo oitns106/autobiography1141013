640d335c70025f66b8404264b8104fcf6d7effdc
oitns106 <oitns106@gmail.com>
2025-10-20 23:26:07 +0800

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+final player=AudioPlayer()..setReleaseMode(ReleaseMode.loop);
 
 void main() {
   runApp(const MyApp());
@@ -41,6 +44,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    if (currentIndex==0) player.play(AssetSource("1.mp3"));
     return Scaffold(
       appBar: AppBar(
         title: Text("我的自傳"),
@@ -57,17 +61,35 @@ class _MyHomePageState extends State<MyHomePage> {
                              iconSize: 30,
                              currentIndex: currentIndex,
                              items: [
-                               BottomNavigationBarItem(icon: Icon(Icons.mail), label:"自我介紹",),
-                               BottomNavigationBarItem(icon: Icon(Icons.school), label:"學習歷程",),
-                               BottomNavigationBarItem(icon: Icon(Icons.scale_outlined), label:"學習計畫",),
-                               BottomNavigationBarItem(icon: Icon(Icons.engineering), label:"專業方向",),
+                               BottomNavigationBarItem(icon: currentIndex==0? Image.asset('assets/a1.png', width: 40, height: 40,):Image.asset('assets/a11.png', width: 30, height: 30,), label:"自我介紹",),
+                               BottomNavigationBarItem(icon: currentIndex==1? Image.asset('assets/a2.png', width: 40, height: 40,):Image.asset('assets/a21.png', width: 30, height: 30,), label:"學習歷程",),
+                               BottomNavigationBarItem(icon: currentIndex==2? Image.asset('assets/a3.jpg', width: 40, height: 40,):Image.asset('assets/a31.jpg', width: 30, height: 30,), label:"學習計畫",),
+                               BottomNavigationBarItem(icon: currentIndex==3? Image.asset('assets/a4.png', width: 40, height: 40,):Image.asset('assets/a41.png', width: 30, height: 30,), label:"專業方向",),
                              ],
                              onTap: (index) {
                                setState(() {
                                  previousIndex=currentIndex;
                                  currentIndex=index;
-                                 if (index==0) {}
-
+                                 if (index==0) {
+                                    if (previousIndex==currentIndex) player.resume();
+                                    player.stop();
+                                    player.play(AssetSource("1.mp3"));
+                                 }
+                                 if (index==1) {
+                                   if (previousIndex==currentIndex) player.resume();
+                                   player.stop();
+                                   player.play(AssetSource("2.mp3"));
+                                 }
+                                 if (index==2) {
+                                   if (previousIndex==currentIndex) player.resume();
+                                   player.stop();
+                                   player.play(AssetSource("3.mp3"));
+                                 }
+                                 if (index==3) {
+                                   if (previousIndex==currentIndex) player.resume();
+                                   player.stop();
+                                   player.play(AssetSource("4.mp3"));
+                                 }
                                });
                              },
                            ),
@@ -76,11 +98,73 @@ class _MyHomePageState extends State<MyHomePage> {
 }
 
 class Screen1 extends StatelessWidget {
-  const Screen1({super.key});
+  Screen1({super.key});
+
+  String s1="我出生在一個很平凡但很美滿的小家庭，父親是個公務員，在台電服務，母親是個家庭主婦，而弟弟和我都還在學校求學。父母用民主的方式管教我們，希望我們能夠獨立自主、主動學習，累積人生經驗，但他們會適時的給予鼓勵和建議，父母親只對我們要求兩件事，第一是保持健康，第二是著重課業。因為沒有健康的身體，就算有再多的才華、再大的抱負也無法發揮出來。又因為家境並不富裕，所以必須專心於課業上，學得一技之長，將來才能自立更生。"
+  "在小學時代的我很活潑、很好動，在課業上表現平平，但課外表現不錯，除了擔任過班長等幹部外，還參加樂隊、糾察隊等，另外還曾獲選為校隊參加跳高比賽。"
+  "小學畢業後，進入了一所私立中學，因為校規嚴格，使原本好動的我變得較為文靜，不過在那裡我學會了許多應有的禮節與待人處世的道理。在國中時期的我，好像開了竅，代表全校接受縣政府的表揚，在國三畢業典禮上，更代表了全體畢業生上台領取畢業證書。"
+  "進附中後，每天都覺得很充實、很快樂。附中學生的特色是能K、能玩，所以我不斷地努力學習，希望能夠達到此目標。在課業方面，我都能保持在一定的水準，因為上課專心聽講、仔細思考、體會老師所說的每一句話，在腦海裡架構重要觀念，一有問題就立刻發問，因此上課的吸收效率很高，不但使得複習的工作能夠很快完成，還有多餘的時間從事課外活動。在這麼多的科目當中，我最喜歡的是數學、化學和生物，因為數學、化學能夠訓練我們組織與思考能力。而生物則和日常生活有密切的關係，且它為我們揭開人體的奧秘。";
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          //標題
+          Padding(
+            padding: EdgeInsets.fromLTRB(30, 20, 30, 20),
+            child: Text("Who am I", style: TextStyle(fontSize: 32,
+                                                     fontWeight: FontWeight.bold),
+                   ),
+          ),
+          //自傳部分
+          Container(
+            padding: EdgeInsets.all(20),
+            margin: EdgeInsets.fromLTRB(30, 0, 30, 30),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.black, width: 3,),
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(color: Colors.amberAccent, offset: Offset(6,6)),
+              ],
+            ),
+            child: Text(s1, style: TextStyle(fontSize: 20,)),
+          ),
+          SizedBox(height: 15),
+          Container(
+            color: Colors.redAccent,
+            child: Image.asset('assets/a1.png'),
+            width: 200,
+            height: 200,
+          ),
+          SizedBox(height: 25),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                width: 150,
+                height: 150,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.purple, width: 2, style: BorderStyle.solid),
+                  borderRadius: BorderRadius.circular(30),
+                  image: DecorationImage(image: AssetImage('assets/a2.png'), fit: BoxFit.cover),
+                ),
+              ),
+              //SizedBox(width: 10,),
+              Container(
+                width: 150,
+                height: 150,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.purple, width: 2, style: BorderStyle.solid),
+                  borderRadius: BorderRadius.circular(30),
+                  image: DecorationImage(image: AssetImage('assets/a3.jpg'), fit: BoxFit.cover),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
 
@@ -89,7 +173,7 @@ class Screen2 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Text('Screen2');
   }
 }
 class Screen3 extends StatelessWidget {
@@ -97,7 +181,43 @@ class Screen3 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("大一時期", style: TextStyle(fontSize: 24,)),
+            ],
+          ),
+          SizedBox(height: 10,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                height: 200,
+                width: 140,
+                //width: 200,
+                child: ListView(
+                  children: [
+                    Text("1. 學好英文", style: TextStyle(fontSize: 20,)),
+                    Text("2. 組合語言", style: TextStyle(fontSize: 20,)),
+                    Text("3. 考取證照", style: TextStyle(fontSize: 20,)),
+                    Text("4. 人際關係", style: TextStyle(fontSize: 20,)),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 10,),
+          Row(),
+          SizedBox(height: 10,),
+          Row(),
+          SizedBox(height: 10,),
+          Row(),
+        ],
+      ),
+    );
   }
 }
 class Screen4 extends StatelessWidget {
@@ -105,6 +225,6 @@ class Screen4 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Text('Screen4');
   }
 }
